ad371ed9e9f94d3cf7383a76567d29f97a790ab2
Mike Landers <mjl9320@gmail.com>
2021-03-15 11:57:21 -0700

    Update ipy_extension.py: add comments

--- ipy_extension.py
+++ ipy_extension.py
@@ -8,9 +8,11 @@ def check_ip_against_cidrs(ip, cidrs):
     for cidr in cidrs:
         try:
             cidr_ip_set = IPSet([IP(cidr)])
+            # if a match has been found, return True and the matching CIDR IPSet
             if not cidr_ip_set.isdisjoint(ip_set_input):
                 return True, cidr_ip_set
         except Exception as e:
             print("Error while checking IP: " + str(ip) + " against CIDR: " + str(cidr))
             raise e
+    # if a match was not found, return False and an empty IPSet
     return False, IPSet()
